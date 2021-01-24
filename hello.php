1dfe971d6d538fc37eef7bbc4bf95efb11cd90d7
marangothan <77908779+marangothan@users.noreply.github.com>
2021-01-23 21:44:59 -0600

    Create hello.php
    PHP HTML file

---
+++ hello.php
@@ -0,0 +1,11 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+<?php
+    echo "<h2>Welcome to NTT DATA!</h2>";
+    echo "Today is " . date("Y/m/d") . "<br>";
+ ?>
+ </body>
+</html>
